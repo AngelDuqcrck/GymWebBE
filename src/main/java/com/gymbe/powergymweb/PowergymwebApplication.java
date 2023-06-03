eacb71e699b131b24aa268f177018d5a849f71a8
Angel Yesid Duque <angelyesidducr@ufps.edu.co>
2023-06-02 23:02:08 -0500

    Initial commit

---
+++ src/main/java/com/gymbe/powergymweb/PowergymwebApplication.java
@@ -0,0 +1,13 @@
+package com.gymbe.powergymweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowergymwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowergymwebApplication.class, args);
+	}
+
+}
